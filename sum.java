e348c8bc1984da0874d37fb2a094f95d0d05d8c1
Arkaprava Ray <rayarka26@gmail.com>
2021-10-08 17:29:07 +0530

    bnn

--- sum.java
+++ sum.java
@@ -2,7 +2,7 @@ class sum{
     public static void main(String [] args){
        
         int a=5, b =10;
-        System.out.println(a+b);
+        System.out.println("THe sum is "+(a+b));
 
 
     }
